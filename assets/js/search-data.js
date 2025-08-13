acd7d22884886affd793b71b929675ad464e0dec
gianluigilopardo <gianluigilopardo@gmail.com>
2025-08-13 14:15:36 +0000

    Deploying to gh-pages from @ gianluigilopardo/gianluigilopardo.github.io@4f54f4f45693ba7ead91213c6d2b08ab3ec23a87 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-corporate-talks",
           title: "Corporate Talks",
-          description: "Interactive visualization of US corporate topic exposures",
+          description: "Interactive visualization of US corporate earnings call topic exposures",
           section: "Navigation",
           handler: () => {
             window.location.href = "/corporatetalks/";
